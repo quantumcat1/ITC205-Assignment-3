8057455b292633bb46750320d1a81a77c192ada3
Erin <thequantumcat@hotmail.com>
2016-08-31 09:51:51 +1000

    adds implementation for Book.java
    does not implement methods involving Loan objects, they are a TODO for later

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -4,63 +4,91 @@ import library.interfaces.entities.EBookState;
 import library.interfaces.entities.IBook;
 import library.interfaces.entities.ILoan;
 
-//just a little change to make sure I can push to the repository properly
-//extra comment to change it further
 
 public class Book implements IBook
 {
+	EBookState state;
+	String author;
+	String title;
+	String callNumber;
+	//bookId, not id - otherwise could get confused with the member Id who is borrowing it.
+	int bookId;
+
 	public void borrow(ILoan loan)
 	{
-
+		//TODO: implement borrow()
 	}
 
 	public ILoan getLoan()
 	{
+		//TODO: implement loan()
 		return null;
 	}
 
 	public void returnBook(boolean damaged)
 	{
-
+		//can't return book unless it was on loan.
+		if(state == EBookState.ON_LOAN)
+		{
+			if(damaged)
+			{
+				state = EBookState.DAMAGED;
+			}
+			else
+			{
+				state = EBookState.AVAILABLE;
+			}
+		}
+		//TODO: add something about changing the loan?
 	}
 
 	public void lose()
 	{
-
+		state = EBookState.LOST;
 	}
 
 	public void repair()
 	{
-
+		//should not become available if it is lost, on loan, or disposed of
+		if(state == EBookState.DAMAGED)
+		{
+			state = EBookState.AVAILABLE;
+		}
+		//TODO: make sure it is not part of a loan
 	}
 
 	public void dispose()
 	{
-
+		//should not be thrown away if it is available, on loan, or lost.
+		if(state == EBookState.DAMAGED)
+		{
+			state = EBookState.DISPOSED;
+		}
+		//TODO: make sure it is not part of a loan
 	}
 
 	public EBookState getState()
 	{
-		return null;
+		return state;
 	}
 
 	public String getAuthor()
 	{
-		return null;
+		return author;
 	}
 
 	public String getTitle()
 	{
-		return null;
+		return title;
 	}
 
 	public String getCallNumber()
 	{
-		return null;
+		return callNumber;
 	}
 
 	public int getId()
 	{
-		return 1;
+		return bookId;
 	}
 }
