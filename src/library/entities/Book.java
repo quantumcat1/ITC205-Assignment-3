77fd519914c6220b68567ae9c775297f025b7095
Erin <thequantumcat@hotmail.com>
2016-08-30 10:25:42 +1000

    adding files

---
+++ src/library/entities/Book.java
@@ -0,0 +1,65 @@
+package library.entities;
+
+import library.interfaces.entities.EBookState;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+
+//just a little change to make sure I can push to the repository properly
+
+public class Book implements IBook
+{
+	public void borrow(ILoan loan)
+	{
+
+	}
+
+	public ILoan getLoan()
+	{
+		return null;
+	}
+
+	public void returnBook(boolean damaged)
+	{
+
+	}
+
+	public void lose()
+	{
+
+	}
+
+	public void repair()
+	{
+
+	}
+
+	public void dispose()
+	{
+
+	}
+
+	public EBookState getState()
+	{
+		return null;
+	}
+
+	public String getAuthor()
+	{
+		return null;
+	}
+
+	public String getTitle()
+	{
+		return null;
+	}
+
+	public String getCallNumber()
+	{
+		return null;
+	}
+
+	public int getId()
+	{
+		return 1;
+	}
+}
---
+++ src/library/interfaces/entities/EBookState.java
@@ -0,0 +1,7 @@
+package library.interfaces.entities;
+
+public enum EBookState
+{
+	AVAILABLE, ON_LOAN, LOST, DAMAGED, DISPOSED
+
+}
