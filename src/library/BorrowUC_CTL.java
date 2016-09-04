b52f5a1cd5e0ceae8d96b4fa90907bda4ee4bee1
Erin <thequantumcat@hotmail.com>
2016-09-04 16:08:18 +1000

    brings in line with entity changes

--- src/library/BorrowUC_CTL.java
+++ src/library/BorrowUC_CTL.java
@@ -12,9 +12,9 @@ import library.interfaces.daos.IBookDAO;
 import library.interfaces.daos.ILoanDAO;
 import library.interfaces.daos.IMemberDAO;
 import library.interfaces.entities.EBookState;
-import library.interfaces.entities.IBook;
-import library.interfaces.entities.ILoan;
-import library.interfaces.entities.IMember;
+import library.entities.Book;
+import library.entities.Loan;
+import library.entities.Member;
 import library.interfaces.hardware.ICardReader;
 import library.interfaces.hardware.ICardReaderListener;
 import library.interfaces.hardware.IDisplay;
@@ -22,30 +22,30 @@ import library.interfaces.hardware.IPrinter;
 import library.interfaces.hardware.IScanner;
 import library.interfaces.hardware.IScannerListener;
 
-public class BorrowUC_CTL implements ICardReaderListener, 
-									 IScannerListener, 
+public class BorrowUC_CTL implements ICardReaderListener,
+									 IScannerListener,
 									 IBorrowUIListener {
-	
+
 	private ICardReader reader;
-	private IScanner scanner; 
-	private IPrinter printer; 
+	private IScanner scanner;
+	private IPrinter printer;
 	private IDisplay display;
 	//private String state;
 	private int scanCount = 0;
 	private IBorrowUI ui;
-	private EBorrowState state; 
+	private EBorrowState state;
 	private IBookDAO bookDAO;
 	private IMemberDAO memberDAO;
 	private ILoanDAO loanDAO;
-	
-	private List<IBook> bookList;
-	private List<ILoan> loanList;
-	private IMember borrower;
-	
+
+	private List<Book> bookList;
+	private List<Loan> loanList;
+	private Member borrower;
+
 	private JPanel previous;
 
 
-	public BorrowUC_CTL(ICardReader reader, IScanner scanner, 
+	public BorrowUC_CTL(ICardReader reader, IScanner scanner,
 			IPrinter printer, IDisplay display,
 			IBookDAO bookDAO, ILoanDAO loanDAO, IMemberDAO memberDAO ) {
 
@@ -53,12 +53,12 @@ public class BorrowUC_CTL implements ICardReaderListener,
 		this.ui = new BorrowUC_UI(this);
 		state = EBorrowState.CREATED;
 	}
-	
+
 	public void initialise() {
 		previous = display.getDisplay();
-		display.setDisplay((JPanel) ui, "Borrow UI");		
+		display.setDisplay((JPanel) ui, "Borrow UI");
 	}
-	
+
 	public void close() {
 		display.setDisplay(previous, "Main Menu");
 	}
@@ -67,15 +67,15 @@ public class BorrowUC_CTL implements ICardReaderListener,
 	public void cardSwiped(int memberID) {
 		throw new RuntimeException("Not implemented yet");
 	}
-	
-	
-	
+
+
+
 	@Override
 	public void bookScanned(int barcode) {
 		throw new RuntimeException("Not implemented yet");
 	}
 
-	
+
 	private void setState(EBorrowState state) {
 		throw new RuntimeException("Not implemented yet");
 	}
@@ -84,7 +84,7 @@ public class BorrowUC_CTL implements ICardReaderListener,
 	public void cancelled() {
 		close();
 	}
-	
+
 	@Override
 	public void scansCompleted() {
 		throw new RuntimeException("Not implemented yet");
@@ -100,13 +100,13 @@ public class BorrowUC_CTL implements ICardReaderListener,
 		throw new RuntimeException("Not implemented yet");
 	}
 
-	private String buildLoanListDisplay(List<ILoan> loans) {
+	private String buildLoanListDisplay(List<Loan> loans) {
 		StringBuilder bld = new StringBuilder();
-		for (ILoan ln : loans) {
+		for (Loan ln : loans) {
 			if (bld.length() > 0) bld.append("\n\n");
 			bld.append(ln.toString());
 		}
-		return bld.toString();		
+		return bld.toString();
 	}
 
 }
--- src/library/Main.java
+++ src/library/Main.java
@@ -12,9 +12,9 @@ import library.interfaces.IMainListener;
 import library.interfaces.daos.IBookDAO;
 import library.interfaces.daos.ILoanDAO;
 import library.interfaces.daos.IMemberDAO;
-import library.interfaces.entities.IBook;
-import library.interfaces.entities.ILoan;
-import library.interfaces.entities.IMember;
+import library.entities.Book;
+import library.entities.Loan;
+import library.entities.Member;
 import library.panels.MainPanel;
 
 public class Main implements IMainListener {
@@ -26,41 +26,41 @@ public class Main implements IMainListener {
 	private IBookDAO bookDAO;
 	private ILoanDAO loanDAO;
 	private IMemberDAO memberDAO;
-	
+
 	public Main() {
 		reader = new CardReader();
 		scanner = new Scanner();
 		printer = new Printer();
 		display = new Display();
-		
+
 		//setupTestData();
 	}
 
 
-	public void showGUI() {		
+	public void showGUI() {
 		reader.setVisible(true);
 		scanner.setVisible(true);
 		printer.setVisible(true);
 		display.setVisible(true);
 	}
 
-	
+
 	@Override
 	public void borrowBooks() {
-		BorrowUC_CTL ctl = new BorrowUC_CTL(reader, scanner, printer, display, 
+		BorrowUC_CTL ctl = new BorrowUC_CTL(reader, scanner, printer, display,
 				 null, null, null);
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
             	ctl.initialise();
             }
-        });		
+        });
 	}
 
-	
+
 	private void setupTestData() {
-        IBook[] book = new IBook[15];
-		IMember[] member = new IMember[6];
-		
+        Book[] book = new Book[15];
+		Member[] member = new Member[6];
+
 		book[0]  = bookDAO.addBook("author1", "title1", "callNo1");
 		book[1]  = bookDAO.addBook("author1", "title2", "callNo2");
 		book[2]  = bookDAO.addBook("author1", "title3", "callNo3");
@@ -76,49 +76,49 @@ public class Main implements IMainListener {
 		book[12] = bookDAO.addBook("author5", "title13", "callNo13");
 		book[13] = bookDAO.addBook("author5", "title14", "callNo14");
 		book[14] = bookDAO.addBook("author5", "title15", "callNo15");
-		
+
 		member[0] = memberDAO.addMember("fName0", "lName0", "0001", "email0");
 		member[1] = memberDAO.addMember("fName1", "lName1", "0002", "email1");
 		member[2] = memberDAO.addMember("fName2", "lName2", "0003", "email2");
 		member[3] = memberDAO.addMember("fName3", "lName3", "0004", "email3");
 		member[4] = memberDAO.addMember("fName4", "lName4", "0005", "email4");
 		member[5] = memberDAO.addMember("fName5", "lName5", "0006", "email5");
-		
+
 		Calendar cal = Calendar.getInstance();
 		Date now = cal.getTime();
-				
-		//create a member with overdue loans		
+
+		//create a member with overdue loans
 		for (int i=0; i<2; i++) {
-			ILoan loan = loanDAO.createLoan(member[1], book[i]);
+			Loan loan = loanDAO.createLoan(member[1], book[i]);
 			loanDAO.commitLoan(loan);
 		}
 		cal.setTime(now);
-		cal.add(Calendar.DATE, ILoan.LOAN_PERIOD + 1);
-		Date checkDate = cal.getTime();		
+		cal.add(Calendar.DATE, Loan.LOAN_PERIOD + 1);
+		Date checkDate = cal.getTime();
 		loanDAO.updateOverDueStatus(checkDate);
-		
+
 		//create a member with maxed out unpaid fines
 		member[2].addFine(10.0f);
-		
+
 		//create a member with maxed out loans
 		for (int i=2; i<7; i++) {
-			ILoan loan = loanDAO.createLoan(member[3], book[i]);
+			Loan loan = loanDAO.createLoan(member[3], book[i]);
 			loanDAO.commitLoan(loan);
 		}
-		
+
 		//a member with a fine, but not over the limit
 		member[4].addFine(5.0f);
-		
+
 		//a member with a couple of loans but not over the limit
 		for (int i=7; i<9; i++) {
-			ILoan loan = loanDAO.createLoan(member[5], book[i]);
+			Loan loan = loanDAO.createLoan(member[5], book[i]);
 			loanDAO.commitLoan(loan);
 		}
 	}
 
-	
+
 	public static void main(String[] args) {
-		
+
         // start the GUI
 		Main main = new Main();
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -129,5 +129,5 @@ public class Main implements IMainListener {
         });
 	}
 
-	
+
 }
