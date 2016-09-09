aa9417c38d92b72ffb6c750369dffe9833146376
Erin <thequantumcat@hotmail.com>
2016-09-09 15:44:37 +1000

    Begins to fix for changes to entities and daos, not finished yet

--- src/library/Main.java
+++ src/library/Main.java
@@ -9,25 +9,24 @@ import java.util.Calendar;
 import java.util.Date;
 
 import library.interfaces.IMainListener;
-import library.interfaces.daos.IBookDAO;
-import library.interfaces.daos.ILoanDAO;
-import library.interfaces.daos.IMemberDAO;
+import library.daos.BookDAO;
+import library.daos.LoanDAO;
+import library.daos.MemberDAO;
+import library.daos.BookDAO;
 import library.entities.Book;
 import library.entities.Loan;
 import library.entities.Member;
 import library.panels.MainPanel;
 
-public class Main implements IMainListener {
-
+public class Main implements IMainListener
+{
 	private CardReader reader;
 	private Scanner scanner;
 	private Printer printer;
 	private Display display;
-	private IBookDAO bookDAO;
-	private ILoanDAO loanDAO;
-	private IMemberDAO memberDAO;
 
-	public Main() {
+	public Main()
+	{
 		reader = new CardReader();
 		scanner = new Scanner();
 		printer = new Printer();
@@ -37,7 +36,8 @@ public class Main implements IMainListener {
 	}
 
 
-	public void showGUI() {
+	public void showGUI()
+	{
 		reader.setVisible(true);
 		scanner.setVisible(true);
 		printer.setVisible(true);
@@ -47,48 +47,54 @@ public class Main implements IMainListener {
 
 	@Override
 	public void borrowBooks() {
-		BorrowUC_CTL ctl = new BorrowUC_CTL(reader, scanner, printer, display,
-				 null, null, null);
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	ctl.initialise();
-            }
-        });
+		BorrowUC_CTL ctl = new BorrowUC_CTL(reader, scanner, printer, display);
+				// null, null, null);
+        javax.swing.SwingUtilities.invokeLater
+        (
+	        new Runnable()
+	        {
+	            public void run() {
+	            	ctl.initialise();
+	            }
+	        }
+        );
 	}
 
 
-	private void setupTestData() {
+	private void setupTestData()
+	{
         Book[] book = new Book[15];
 		Member[] member = new Member[6];
 
-		book[0]  = bookDAO.addBook("author1", "title1", "callNo1");
-		book[1]  = bookDAO.addBook("author1", "title2", "callNo2");
-		book[2]  = bookDAO.addBook("author1", "title3", "callNo3");
-		book[3]  = bookDAO.addBook("author1", "title4", "callNo4");
-		book[4]  = bookDAO.addBook("author2", "title5", "callNo5");
-		book[5]  = bookDAO.addBook("author2", "title6", "callNo6");
-		book[6]  = bookDAO.addBook("author2", "title7", "callNo7");
-		book[7]  = bookDAO.addBook("author2", "title8", "callNo8");
-		book[8]  = bookDAO.addBook("author3", "title9", "callNo9");
-		book[9]  = bookDAO.addBook("author3", "title10", "callNo10");
-		book[10] = bookDAO.addBook("author4", "title11", "callNo11");
-		book[11] = bookDAO.addBook("author4", "title12", "callNo12");
-		book[12] = bookDAO.addBook("author5", "title13", "callNo13");
-		book[13] = bookDAO.addBook("author5", "title14", "callNo14");
-		book[14] = bookDAO.addBook("author5", "title15", "callNo15");
+		book[0]  = BookDAO.getInstance().add(new Book("author1", "title1", "callNo1"));
+		book[1]  = BookDAO.getInstance().add(new Book("author1", "title2", "callNo2"));
+		book[2]  = BookDAO.getInstance().add(new Book("author1", "title3", "callNo3"));
+		book[3]  = BookDAO.getInstance().add(new Book("author1", "title4", "callNo4"));
+		book[4]  = BookDAO.getInstance().add(new Book("author2", "title5", "callNo5"));
+		book[5]  = BookDAO.getInstance().add(new Book("author2", "title6", "callNo6"));
+		book[6]  = BookDAO.getInstance().add(new Book("author2", "title7", "callNo7"));
+		book[7]  = BookDAO.getInstance().add(new Book("author2", "title8", "callNo8"));
+		book[8]  = BookDAO.getInstance().add(new Book("author3", "title9", "callNo9"));
+		book[9]  = BookDAO.getInstance().add(new Book("author3", "title10", "callNo10"));
+		book[10] = BookDAO.getInstance().add(new Book("author4", "title11", "callNo11"));
+		book[11] = BookDAO.getInstance().add(new Book("author4", "title12", "callNo12"));
+		book[12] = BookDAO.getInstance().add(new Book("author5", "title13", "callNo13"));
+		book[13] = BookDAO.getInstance().add(new Book("author5", "title14", "callNo14"));
+		book[14] = BookDAO.getInstance().add(new Book("author5", "title15", "callNo15"));
 
-		member[0] = memberDAO.addMember("fName0", "lName0", "0001", "email0");
-		member[1] = memberDAO.addMember("fName1", "lName1", "0002", "email1");
-		member[2] = memberDAO.addMember("fName2", "lName2", "0003", "email2");
-		member[3] = memberDAO.addMember("fName3", "lName3", "0004", "email3");
-		member[4] = memberDAO.addMember("fName4", "lName4", "0005", "email4");
-		member[5] = memberDAO.addMember("fName5", "lName5", "0006", "email5");
+		member[0] = MemberDAO.getInstance().add(new Member("fName0", "lName0", "0001", "email0"));
+		member[1] = MemberDAO.getInstance().add(new Member("fName1", "lName1", "0002", "email1"));
+		member[2] = MemberDAO.getInstance().add(new Member("fName2", "lName2", "0003", "email2"));
+		member[3] = MemberDAO.getInstance().add(new Member("fName3", "lName3", "0004", "email3"));
+		member[4] = MemberDAO.getInstance().add(new Member("fName4", "lName4", "0005", "email4"));
+		member[5] = MemberDAO.getInstance().add(new Member("fName5", "lName5", "0006", "email5"));
 
 		Calendar cal = Calendar.getInstance();
 		Date now = cal.getTime();
 
 		//create a member with overdue loans
-		for (int i=0; i<2; i++) {
+		for (int i=0; i<2; i++)
+		{
 			Loan loan = loanDAO.createLoan(member[1], book[i]);
 			loanDAO.commitLoan(loan);
 		}
@@ -101,7 +107,8 @@ public class Main implements IMainListener {
 		member[2].addFine(10.0f);
 
 		//create a member with maxed out loans
-		for (int i=2; i<7; i++) {
+		for (int i=2; i<7; i++)
+		{
 			Loan loan = loanDAO.createLoan(member[3], book[i]);
 			loanDAO.commitLoan(loan);
 		}
@@ -117,7 +124,8 @@ public class Main implements IMainListener {
 	}
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
         // start the GUI
 		Main main = new Main();
